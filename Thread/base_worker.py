d11cb03b8f03ca07223a081eb8a1769e148e9d57
Mateusz Szynka <devmsw12@gmail.com>
2016-05-11 13:55:25 +0200

    Infinite loop fix

--- Thread/base_worker.py
+++ Thread/base_worker.py
@@ -1,4 +1,3 @@
-import threading
 from abc import abstractmethod
 from timeit import default_timer as timer
 
@@ -6,21 +5,21 @@ from Base.base_class import BaseClass
 from Mediator.mediator import Mediator
 
 
-class BaseWorker(BaseClass, threading.Thread):
+class BaseWorker(BaseClass):
 	def __init__ (self, mediator: Mediator, parent):
 		super().__init__()
-		threading.Thread.__init__(self)
 		self._parent = parent
 		self.mediator = mediator
 
 	def run (self):
 		self.logger.debug("Running")
-		keep_worker = True
-		while keep_worker:
-			start = timer()
-			keep_worker = self.task()
-			end = timer()
-			self.logger.debug("Finished processing. Elapsed: %.2f s", end - start)
+
+		start = timer()
+		keep_worker = self.task()
+		end = timer()
+
+		self.logger.debug("Finished processing. Elapsed: %.2f s", end - start)
+		return keep_worker
 
 	@abstractmethod
 	def task (self) -> bool:
