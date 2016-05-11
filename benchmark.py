1c564146c5f250aff8a8c171b2a685ea8bf7e8e4
Mateusz Szynka <devmsw12@gmail.com>
2016-05-11 14:31:37 +0200

    Benchmarking notes

--- benchmark.py
+++ benchmark.py
@@ -1,11 +1,14 @@
-# from main import Main
+from pytest_benchmark.plugin import benchmark
+
+from main import Main
 
 
-#  benchmark.pedantic(sth, setup=my_special_setup, args=(1, 2, 3), kwargs={'foo': 'bar'}, iterations=10,rounds = 100)
-
-# def test_foo ():
-# 	webcrawler = Main(7)
-# 	benchmark.pedantic(Main.run, args=webcrawler, iterations=2, rounds=10)
+def test_foo ():
+	webcrawler = Main(7)
+	benchmark.pedantic(webcrawler.run, iterations=10, rounds=10)
 
 
-# test_foo()
+test_foo()
+
+# http://matplotlib.org
+# TODO: implement benchmarking tool
